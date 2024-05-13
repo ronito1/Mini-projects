2f731b0c9657ffef9e81f339e7c03d81969a492d
Ronit Pathak <91935063+ronito1@users.noreply.github.com>
2024-05-13 21:26:21 +0530

    Add files via upload

---
+++ catnbulb.js
@@ -0,0 +1,11 @@
+function switchOff() {
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+}
+
+function switchOn() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+
+
+}
